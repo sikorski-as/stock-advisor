fc80b985e899094e86ecb0d7b968e96f844ec48b
A.Sikorski <sikorski.aps@gmail.com>
2020-05-14 21:05:24 +0200

    Added mockup of EA in strategy agent

--- strategy_agent.py
+++ strategy_agent.py
@@ -1,5 +1,9 @@
+import asyncio
+import math
+import random
 from asyncio import sleep
 
+import numpy as np
 from aioxmpp import PresenceShow
 from spade import agent
 from spade.behaviour import OneShotBehaviour, CyclicBehaviour
@@ -15,21 +19,61 @@ class StrategyAgent(agent.Agent):
         super().__init__(jid, password, verify_security)
         self.currency_symbol = currency_symbol
         self.log = make_logger(self.jid)
+        self.training_behaviour = None
 
     async def setup(self):
         self.log.debug('Starting!')
         self.presence.approve_all = True
-        self.add_behaviour(self.TrainBehaviour())
+        self.training_behaviour = self.TrainBehaviour()
+        self.add_behaviour(self.training_behaviour)
 
     class TrainBehaviour(OneShotBehaviour):
+        def __init__(self, *args, **kwargs):
+            super(StrategyAgent.TrainBehaviour, self).__init__()
+            self.cost_functions_ready = asyncio.Event()
+            self.cost_functions = {}
+            self.workers = [
+                'strategy_agent_worker@localhost'
+            ]
+
         async def on_start(self):
             self.presence.set_available(show=PresenceShow.DND)
 
         async def run(self):
             self.agent.log.debug('Starting training!')
-            await sleep(5)
+
+            population_size = 6
+            mutation_chance = 0.05
+            niterations = 20
+            random_genotype = lambda: np.random.randint(1, 6, size=(2,))
+            repair = lambda genes: np.where(genes < 1, 1, genes)
+            _mutate = lambda genes: genes + np.random.randint(-1, 2, size=(2,))
+            mutate = lambda genes: repair(_mutate(genes))
+
+            population = [random_genotype() for _ in range(population_size)]
+            for i in range(niterations):
+                self.agent.log.debug(f'Starting iteration {i}')
+                population.extend([
+                    mutate(genotype) if np.random.random() > mutation_chance
+                    else genotype
+                    for genotype in population
+                ])
+                costs = await self.compute_cost_function(population)
+                population = [genotype for (genotype, cost) in sorted(zip(population, costs), key=lambda x: x[1])]
+                population = population[:population_size]
+
             self.agent.log.debug('Training done!')
             self.agent.add_behaviour(StrategyAgent.GiveDecisionBehaviour(), request_decision_template)
+
+        async def compute_cost_function(self, population):
+            return [0 for _ in population]
+
+    class WorkerConversationBehaviour(OneShotBehaviour):
+        async def on_start(self):
+            pass
+
+        async def run(self):
+            pass
 
     class GiveDecisionBehaviour(CyclicBehaviour):
         async def on_start(self):
