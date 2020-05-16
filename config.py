6c5053ea5023ae2bcf914ee5efafdd36744cd1d1
sikorski-as <sikorski-as@users.noreply.github.com>
2020-05-16 21:28:32 +0200

    Config.py cleared

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
-domain = "desktop-jcug7jt"
-DB_PATH = "E:\OneDrive - Politechnika Warszawska\Pulpit\stock-advisor\\"
+domain = ""
+
+DB_PATH = ""
 API_KEY = ""
+
 timeout = 100
+
 TRAIN_OPERATION = "Train"
 DECISION_OPERATION = "Decision"
 LIST_OPERATION = "List"
